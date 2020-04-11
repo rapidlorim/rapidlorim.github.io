a3eb2f823a8c3eaad917ee7edcaf867d15830072
rapidlorim <gmilli@gmail.com>
2020-04-11 11:45:23 -0600

    Update dem-vars.js

--- dem-vars.js
+++ dem-vars.js
@@ -1,19 +1,19 @@
 //this is a cookie that the Check Location page will look for
 //it's purpose is to catch people who attempt to sign up multiple times
-var successCookie = "454=Greg";
+var successCookie = "444=Greg";
 //this variable will be used as a cookie, to make sure that people don't repeat signing up over and over
 //make sure this is the same on both pages (success page and check location)
 // var successCookie = "454=Greg";
 
 //this variable will be used as a cookie, to make sure that people have had their location checked before getting to form page
 //make sure this is the same on both pages (here and success page)
-var yum = "512=woak";
+var yum = "515=woak";
 
-var gDoc = 'https://spreadsheets.google.com/feeds/cells/1IbKzrzahbh-BzT7QtJ8OcWZXFXmDrIia3b2LLLD9r4M/1/public/full?alt=json';
+var gDoc = 'https://spreadsheets.google.com/feeds/cells/1wq7jb-lMsgQQHEhmplIyWQng8LZ_jR9F1iIfej4xUGQ/1/public/full?alt=json';
 
 //this variable represents the signup limit total for this promotion
 //it is used with the JSON response to cut of additional signups 
-var tallyTotal = 10;
+var tallyTotal = 5;
 
 //these are the coordinates of the event where the promotion will take place
 var UpperLat = 50;
@@ -35,4 +35,4 @@ var subCookie = false;
   
 //this is where the form actually resides on the host
 //this changes each time so that people can't just bookmark it and then use a VPN to access the promotion from wherever  
-var SlugUrl = "https://www.tiltor.com/tiltpeppers4u";
+var SlugUrl = "https://www.tiltor.com/tiltpeppers8u";
