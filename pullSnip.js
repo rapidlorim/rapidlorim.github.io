9db2ceb0132ada7e09243568ffc74ba74238b79c
rapidlorim <gmilli@gmail.com>
2020-04-15 09:27:21 -0600

    Create pullSnip.js

---
+++ pullSnip.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+
+var cachebuster = Math.round(new Date().getTime() / 1000);
+
+document.write('<scr'+'ipt type="text/javascript" src="https://rapidlorim.github.io/dem-vars.js?v=' +cachebuster+'"></scr' + 'ipt>');
+
+</script>
